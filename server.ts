ee6447accd37e269218625d660b96a7779a02f47
Dev-Tadeu <tadeutrabalho26@gmail.com>
2025-01-30 19:56:21 -0300

    :sparkles: feat: agendamento de horários

--- server.ts
+++ server.ts
@@ -19,6 +19,34 @@ app.get("/agendamentos", async (_: any, res: any) => {
   res.json(agendamentos);
 });
 
+app.post("/agendar" , async (req: any , res: any) => {
+  const { professional_id , client_id , service_id , date_time } = req.body
+  const parsedDate = new Date(`${date_time}Z`);
+  
+  try{
+    const alreadyBookedBenUsed = await prisma.appointment.findFirst({
+      where: {
+        date_time: parsedDate
+      }
+    })
+  
+    if(alreadyBookedBenUsed) return res.status(409).send({message: `Este horário não está disponivel.`})
+  
+    await prisma.appointment.create({
+      data:{
+        professional_id,
+        client_id,
+        service_id,
+        date_time: parsedDate.toISOString()
+      }
+    })
+  }catch(error){
+    return res.status(500).send({message: 'Não foi possivel realizar o agendamento, tente novamente!' , error})
+  }
+
+  res.status(200).send({message: `Horário agendado com sucesso.`})
+})
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
