d11a15d8beb908df21adf492e2141fba1ab266d7
Dev-Tadeu <tadeutrabalho26@gmail.com>
2025-02-02 21:53:12 -0300

    :sparkles: feat: remarca horários

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
@@ -13,39 +13,101 @@ app.get("/agendamentos", async (_: any, res: any) => {
     include: {
       clientes_appointments_client_idToclientes: true,
       clientes_appointments_professional_idToclientes: true,
-      services: true
-    }
+      services: true,
+    },
   });
   res.json(agendamentos);
 });
 
-app.post("/agendar" , async (req: any , res: any) => {
-  const { professional_id , client_id , service_id , date_time } = req.body
+//agendar
+app.post("/agendar", async (req: any, res: any) => {
+  const { professional_id, client_id, service_id, date_time } = req.body;
   const parsedDate = new Date(`${date_time}Z`);
-  
-  try{
+
+  try {
     const alreadyBookedBenUsed = await prisma.appointment.findFirst({
       where: {
-        date_time: parsedDate
-      }
-    })
-  
-    if(alreadyBookedBenUsed) return res.status(409).send({message: `Este horário não está disponivel.`})
-  
+        date_time: parsedDate,
+      },
+    });
+
+    if (alreadyBookedBenUsed)
+      return res
+        .status(409)
+        .send({ message: `Este horário não está disponivel.` });
+
     await prisma.appointment.create({
-      data:{
+      data: {
         professional_id,
         client_id,
         service_id,
-        date_time: parsedDate.toISOString()
-      }
-    })
-  }catch(error){
-    return res.status(500).send({message: 'Não foi possivel realizar o agendamento, tente novamente!' , error})
+        date_time: parsedDate.toISOString(),
+      },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({
+        message: "Não foi possivel realizar o agendamento, tente novamente!",
+        error,
+      });
   }
 
-  res.status(200).send({message: `Horário agendado com sucesso.`})
-})
+  res.status(200).send({ message: `Horário agendado com sucesso.` });
+});
+
+//remarcar horário
+app.put("/agendar/:id", async (req: any, res: any) => {
+  const id = Number(req.params.id);
+  console.log(id);
+
+  try {
+    const searchId = await prisma.appointment.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!searchId)
+      return res
+        .status(404)
+        .send({
+          message:
+            " Agendamento não encontrado, verifique se realmente foi agendado horário!",
+        });
+
+    const data = { ...req.body };
+    data.date_time = data.date_time ? new Date(`${data.date_time}Z`) : undefined;
+
+    const alreadyBookedBenUsed = await prisma.appointment.findFirst({
+      where: {
+        date_time: data.date_time
+      },
+    });
+
+    if (alreadyBookedBenUsed)
+      return res
+        .status(409)
+        .send({ message: `Este horário não está disponivel.` });
+
+    await prisma.appointment.update({
+      where: {
+        id
+      },
+      data: data
+    });
+
+  } catch (error) {
+    return res
+      .status(500)
+      .send({
+        message: "Não foi possivel remarcar o horário, tente novamente!",
+        error,
+      });
+  }
+
+  res.status(200).send({ message: `Horário remarcado com sucesso.` });
+});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
